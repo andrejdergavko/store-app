d4e0c7036822885ab8b5aa4d6431ed25a3bc1c7f
Andrei Dergavko <andrejdergavko@gmail.com>
2021-04-18 11:47:17 +0300

    add base web

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled } from 'baseui';
+import { Button } from 'baseui/button';
+
+const engine = new Styletron();
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Button>Hello</Button>
+      </BaseProvider>
+    </StyletronProvider>
+  );
 }
