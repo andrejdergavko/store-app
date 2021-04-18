a8d493e16e944a17f2c6526a2f6aa8a4cf3ceaaa
Andrei Dergavko <andrejdergavko@gmail.com>
2021-04-18 13:33:51 +0300

    add redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Client as Styletron } from 'styletron-engine-atomic';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { LightTheme, BaseProvider, styled } from 'baseui';
 import { Button } from 'baseui/button';
 
+import store from './store/store';
+
 const engine = new Styletron();
 
 export default function App() {
   return (
-    <StyletronProvider value={engine}>
-      <BaseProvider theme={LightTheme}>
-        <Button>Hello</Button>
-      </BaseProvider>
-    </StyletronProvider>
+    <Provider store={store}>
+      <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+          <Button>Hello</Button>
+        </BaseProvider>
+      </StyletronProvider>
+    </Provider>
   );
 }
